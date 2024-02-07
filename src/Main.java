d0a8470ed00c69520148e98f44f5a6f849619d40
Dan <stickbugdanny@gmail.com>
2024-02-07 02:21:32 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
+        String startFilePath = "C:\\Users\\armad\\OneDrive\\Desktop\\IntelliJ IDEA Community Edition 2021.1.1\\IdeaProjects\\InfoSearch_Pract2_BoolSearch\\src\\res";
 
         long timeM =  System.nanoTime();
-        Matrix m = new Matrix("C:\\Users\\armad\\OneDrive\\Desktop\\IntelliJ IDEA Community Edition 2021.1.1\\IdeaProjects\\InfoSearch_Pract2_BoolSearch\\src\\res");
+        Matrix m = new Matrix(startFilePath);
         long endTimeM = System.nanoTime();
         long elapsedTimeM = endTimeM - timeM; //final time it took
 
         long timeI =  System.nanoTime();
-        Index ind = new Index("C:\\Users\\armad\\OneDrive\\Desktop\\IntelliJ IDEA Community Edition 2021.1.1\\IdeaProjects\\InfoSearch_Pract2_BoolSearch\\src\\res");
+        Index ind = new Index(startFilePath);
         long endTimeI = System.nanoTime();
-        long elapsedTimeI = endTimeI - timeI;
+        long elapsedTimeI = endTimeI - timeI;//final time it took
 
-        System.out.println("\n1.Print matrix;\n2 - Print index;\n3.Show matrix in file;\n4.Show index in file;\n5 - Show time for matrix and time for index;\n6 - Search matrix;\n7 - Search input;\n-1 - Exit\n");
+        System.out.println("\n1 - Print matrix;\n2 - Print index;\n3.Show matrix in file;\n4 - Show index in file;\n5 - Show time for matrix and time for index;\n6 - Search matrix;\n7 - Search index;\n-1 - Exit\n");
         int i= in.nextInt();
 
         while(i!=-1) {
             switch (i) {
                 case 1:
-                    m.print();
+                    System.out.println(m.matrixStats());
                     break;
                 case 2:
-                    ind.print();
+                    System.out.println(ind.indexStats());
                     break;
                 case 3:
                     System.out.println("MATRIX FILE OPENING IN PROGRESS");
                     break;
                 case 4:
-                    System.out.println("INDEX FILE OPENING IN PROGRESS");
+                   // System.out.println("INDEX FILE OPENING IN PROGRESS");
+                    ind.openIndexTXT("src/results/index.txt");
                     break;
                 case 5:
                     System.out.println("Time to build matrix: " + elapsedTimeM + " ns, or " + elapsedTimeM / 1_000_000.0 + " ms, or " + elapsedTimeM / 1_000_000_000.0 + " s");
@@ -55,7 +57,7 @@ public class Main {
                 default:
                     System.out.println("Wrong format");
             }
-            System.out.println("\n1.Print matrix;\n2 - Print index;\n3.Show matrix in file;\n4.Show index in file;\n5 - Show time for matrix and time for index;\n6 - Search matrix;\n7 - Search input;\n-1 - Exit\n");
+            System.out.println("\n1 - Print matrix;\n2 - Print index;\n3.Show matrix in file;\n4 - Show index in file;\n5 - Show time for matrix and time for index;\n6 - Search matrix;\n7 - Search index;\n-1 - Exit\n");
 
             i = in.nextInt();
         }
